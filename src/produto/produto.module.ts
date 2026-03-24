bcbbdbfed9c0949582cc1ed1a387cdbd2b276350
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-03-24 14:58:30 -0300

    🐛 FIX: Correção do Módulo de Categoria

--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CategoriaModule } from "../categoria/categoria.module";
 import { ProdutoController } from "./controllers/produto.controller";
 import { Produto } from "./entities/produto.entity";
 import { ProdutoService } from "./services/produto.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { CategoriaService } from "../categoria/services/categoria.service";
-import { CategoriaModule } from "../categoria/categoria.module";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Produto]), CategoriaModule],
